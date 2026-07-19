46b2ed802c33183b12e363cb8c2b2d96439caf74
Fable-Dzx <18982060830@163.com>
2026-07-19 22:06:38 +0800

    Add files via GitMob upload

---
+++ src/client/kotlin/com/fabledzx/client/MyFirstModDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.fabledzx.client
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object MyFirstModDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
