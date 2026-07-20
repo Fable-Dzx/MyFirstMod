a1b229b6f3bf245cf7f8c00609bfc7f16a6f32f5
Fable-Dzx <18982060830@163.com>
2026-07-20 18:41:21 +0800

    修复MyFirstMod使用1.19.2标准Registry API

--- src/main/java/com/fabledzx/MyFirstMod.java
+++ src/main/java/com/fabledzx/MyFirstMod.java
@@ -1,18 +1,17 @@
 package com.fabledzx;
 
-import net.minecraft.util.Identifier;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
-public class MyFirstMod implements ModInitializer{
+public class MyFirstMod implements ModInitializer {
 
     public static final Item CUSTOM_ITEM = new Item(new FabricItemSettings());
     
     @Override
     public void onInitialize() {
-        Registry.register(Registries.ITEM,new Identifier("fable","custom_item"), CUSTOM_ITEM);
+        Registry.register(Registry.ITEM, new Identifier("fable", "custom_item"), CUSTOM_ITEM);
     }
 }
