a67350192fb56d1ea0b097e604c85a9fd28bd84f
Fable-Dzx <18982060830@163.com>
2026-07-21 15:17:01 +0800

    修复1.19.2注册API路径

--- src/main/java/com/fabledzx/MyFirstMod.java
+++ src/main/java/com/fabledzx/MyFirstMod.java
@@ -4,14 +4,16 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.Registry;   // ← 注意这里
 
 public class MyFirstMod implements ModInitializer {
-
+    public static final String MOD_ID = "myfirstmod";
     public static final Item CUSTOM_ITEM = new Item(new FabricItemSettings());
-    
+
     @Override
     public void onInitialize() {
-        Registry.register(Registry.ITEM, new Identifier("fable", "custom_item"), CUSTOM_ITEM);
+        // 使用 Registry.ITEM 而不是 Registries.ITEM
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "custom_item"), CUSTOM_ITEM);
+        System.out.println("模组初始化完成！");
     }
 }
